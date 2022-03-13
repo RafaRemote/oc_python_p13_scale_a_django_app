259d94d5545be4e87ee2baf22f21f58b33126768
rafaRemote <raphael.49410@gmail.com>
2022-03-13 14:05:54 +0100

    migrations done correctly

---
+++ profiles/migrations/0002_auto_20220313_1258.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+
+def move_profiles(apps, schema_editor):
+    OldProfile = apps.get_model('oc_lettings_site', 'Profile')
+    NewProfile = apps.get_model('profiles', 'Profile')
+
+    objs = list()
+
+
+    for old_object in OldProfile.objects.all():
+        old_favorite_city = old_object.favorite_city
+        old_user_id = old_object.user_id
+
+        new_profile = NewProfile(
+            favorite_city = old_favorite_city, 
+            user_id = old_user_id
+            )
+        objs.append(new_profile)
+
+    NewProfile.objects.bulk_create(objs)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profiles', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(move_profiles, migrations.RunPython.noop),
+    ]
